745a657b648855be785fa54f52e283438956c26d
Sneha P Pratap <snehapratap248@gmail.com>
2025-01-14 00:04:17 +0530

    Update etl.py

--- dags/etl.py
+++ dags/etl.py
@@ -15,14 +15,11 @@ with DAG(
 
 ) as dag:
     
-    ## step 1: Create the table if it doesnt exists
 
     @task
     def create_table():
-        ## initialize the Postgreshook
         postgres_hook=PostgresHook(postgres_conn_id="my_postgres_connection")
 
-        ## SQL query to create the table
         create_table_query="""
         CREATE TABLE IF NOT EXISTS apod_data (
             id SERIAL PRIMARY KEY,
@@ -35,25 +32,21 @@ with DAG(
 
 
         """
-        ## Execute the table creation query
         postgres_hook.run(create_table_query)
 
 
-    ## Step 2: Extract the NASA API Data(APOD)-Astronomy Picture of the Day[Extract pipeline]
-    ## https://api.nasa.gov/planetary/apod?api_key=zMz9WTAAILw4QPyLiKrxhkh3PALVGafuf65wZ555
 
     extract_apod=SimpleHttpOperator(
         task_id='extract_apod',
-        http_conn_id='nasa_api',  ## Connection ID Defined In Airflow For NASA API
-        endpoint='planetary/apod', ## NASA API enpoint for APOD
+        http_conn_id='nasa_api',  
+        endpoint='planetary/apod', 
         method='GET',
-        data={"api_key":"{{ conn.nasa_api.extra_dejson.api_key}}"}, ## USe the API Key from the connection
-        response_filter=lambda response:response.json(), ## Convert response to json
+        data={"api_key":"{{ conn.nasa_api.extra_dejson.api_key}}"},
+        response_filter=lambda response:response.json(), 
     )
 
     
 
-    ## Step 3: Transform the data(Pick the information that i need to save)
     @task
     def transform_apod_data(response):
         apod_data={
@@ -67,20 +60,16 @@ with DAG(
         return apod_data
 
 
-    ## step 4:  Load the data into Postgres SQL
     @task
     def load_data_to_postgres(apod_data):
-        ## Initialize the PostgresHook
         postgres_hook=PostgresHook(postgres_conn_id='my_postgres_connection')
 
-        ## Define the SQL Insert Query
 
         insert_query = """
         INSERT INTO apod_data (title, explanation, url, date, media_type)
         VALUES (%s, %s, %s, %s, %s);
         """
 
-        ## Execute the SQL Query
 
         postgres_hook.run(insert_query,parameters=(
             apod_data['title'],
@@ -94,16 +83,11 @@ with DAG(
 
 
 
-    ## step 5: Verify the data DBViewer
 
 
-    ## step 6: Define the task dependencies
-    ## Extract
-    create_table() >> extract_apod  ## Ensure the table is create befor extraction
+    create_table() >> extract_apod  
     api_response=extract_apod.output
-    ## Transform
     transformed_data=transform_apod_data(api_response)
-    ## Load
     load_data_to_postgres(transformed_data)
 
     
